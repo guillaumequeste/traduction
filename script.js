3dec9fe97c179f914880881a50c20ae38e9e2f7f
Guillaume Queste <guillaume.queste@laposte.net>
2019-11-08 10:06:46 +0100

    update 8 novembre 2019

--- script.js
+++ script.js
@@ -89,6 +89,28 @@ document.querySelector('#translateIt').addEventListener('click', function() {
     }
 }, false);
 
+document.querySelector('#translateDe').addEventListener('click', function() {
+    var xhr = new XMLHttpRequest(),
+        textAPI = document.querySelector('#source').value,
+        langAPI = document.querySelector('#translateDe').value,
+        data = "key=" + keyAPI + "&text=" + textAPI + "&lang=" + langAPI;
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var res = this.responseText;
+            document.querySelector('#json').innerHTML = res;
+            var json = JSON.parse(res);
+            if (json.code == 200) {
+                document.querySelector('#output').innerHTML = json.text[0];
+            } else {
+                document.querySelector('#output').innerHTML = "Error Code: " + json.code;
+            }
+        }
+    }
+}, false);
+
 document.querySelector('#translateFr').addEventListener('click', function() {
     var xhr = new XMLHttpRequest(),
         textAPI = document.querySelector('#source').value,
